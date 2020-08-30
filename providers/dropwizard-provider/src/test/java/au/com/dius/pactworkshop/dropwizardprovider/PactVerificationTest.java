4437bc92a597679182a69344c44d56ac370ea270
Ilgam Shakirov <shilgam@gmail.com>
2020-08-30 10:43:14 +0300

    Undo changes for step 13

--- providers/dropwizard-provider/src/test/java/au/com/dius/pactworkshop/dropwizardprovider/PactVerificationTest.java
+++ providers/dropwizard-provider/src/test/java/au/com/dius/pactworkshop/dropwizardprovider/PactVerificationTest.java
@@ -4,8 +4,8 @@ import au.com.dius.pact.provider.junit.PactRunner;
 import au.com.dius.pact.provider.junit.target.HttpTarget;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
-import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
-import au.com.dius.pact.provider.junitsupport.loader.PactBrokerAuth;
+//import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+//import au.com.dius.pact.provider.junitsupport.loader.PactBrokerAuth;
 import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
 import au.com.dius.pact.provider.junitsupport.target.Target;
 import au.com.dius.pact.provider.junitsupport.target.TestTarget;
@@ -17,8 +17,8 @@ import org.junit.runner.RunWith;
 @RunWith(PactRunner.class)
 @Provider("Our Provider")
 @PactFolder("build/pacts")
-// @PactBroker(host = "test.pact.dius.com.au", scheme = "https", port = "443",
-//  authentication = @PactBrokerAuth(username = "${pactBrokerUser}", password = "${pactBrokerPassword}"))
+//@PactBroker(host = "test.pact.dius.com.au", scheme = "https", port = "443",
+//        authentication = @PactBrokerAuth(username = "${pactBrokerUser}", password = "${pactBrokerPassword}"))
 public class PactVerificationTest {
   @ClassRule
   public static final DropwizardAppRule<ServiceConfig> RULE = new DropwizardAppRule<ServiceConfig>(MainApplication.class,
